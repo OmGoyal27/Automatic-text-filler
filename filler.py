15482a93fc0aca58ad8782d5d7e47f8209a859ef
OmGoyal27 <omgoyal2711@gmail.com>
2024-05-16 17:39:04 +0530

    Add files via upload

--- filler.py
+++ filler.py
@@ -1,19 +1,19 @@
 import pyautogui as pg
 import time
 
-data = ""
+data = "" # Initiate the data variable
 
-def fill():
+def fill():                 # Fill the data
     global data
     time.sleep(0.35)
     pg.write(data)
 
-def prompt():
+def prompt():               # Asks to change the data
     data = pg.prompt(text="Please enter the data you want to make a shortcut of.", title="Shortcut maker")
     if data:
         return data
 
 
-def update_data():
+def update_data():          # Update the data
     global data
     data = prompt()
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import keyboard
-from filler import fill, update_data
+from filler import fill, update_data                        # Imports the actual functions
 
 # Register the hotkey
-keyboard.add_hotkey('right shift', fill)
-keyboard.add_hotkey('right shift+shift', update_data)
+keyboard.add_hotkey('right shift', fill)                    # Hooks the right shift key(Which is one of the most useless keys) to have a function of paste
+keyboard.add_hotkey('right shift+shift', update_data)       # Asks to update data when both "shifts" are pressed 
 
 # Block until ESC is pressed
 input("Hit enter to exit.")
